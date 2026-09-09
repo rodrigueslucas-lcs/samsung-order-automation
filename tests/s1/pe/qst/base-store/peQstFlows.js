43b81bcb60a42298a9cd9f4aefcc6b9e54a4f8be
Lucas Rodrigues <afolucas@gmail.com>
2026-09-09 12:28:11 -0300

    refactor(pe): add reusable safe payment checkpoints

--- tests/s1/pe/qst/base-store/peQstFlows.js
+++ tests/s1/pe/qst/base-store/peQstFlows.js
@@ -2,6 +2,7 @@ import ProductPage from "../../../../../pages/ProductPage";
 import CartPage from "../../../../../pages/CartPage";
 import GuestLoginPage from "../../../../../pages/GuestLoginPage";
 import CheckoutPage from "../../../../../pages/CheckoutPage";
+import PaymentPage from "../../../../../pages/PaymentPage";
 import { testData } from "../../../../../utils/testData";
 
 export async function addConfiguredProductToPeCart(page, config) {
@@ -33,6 +34,17 @@ export async function reachPeGuestDelivery(page, config) {
   return { cart, checkout };
 }
 
+export async function reachPeGuestPayment(page, config, options = {}) {
+  const { cart, checkout } = await reachPeGuestDelivery(page, config);
+  await checkout.fillAddress(options.address || testData.address);
+  await checkout.selectShippingMethod();
+  await checkout.acceptTerms();
+  await checkout.continueToPayment({ expectedPaymentMode: options.expectedPaymentMode });
+  const payment = new PaymentPage(page);
+  await payment.validatePaymentPage({ expectedPaymentMode: options.expectedPaymentMode });
+  return { cart, checkout, payment };
+}
+
 export async function reachPeRegisteredDelivery(page, config) {
   const cart = await addConfiguredProductToPeCart(page, config);
   await cart.proceedToAuthenticatedCheckout();
@@ -40,4 +52,30 @@ export async function reachPeRegisteredDelivery(page, config) {
   const checkout = new CheckoutPage(page);
   await checkout.fillCustomerData(testData.customer);
   return { cart, checkout };
+}
+
+export async function reachPeRegisteredPayment(page, config, options = {}) {
+  const { cart, checkout } = await reachPeRegisteredDelivery(page, config);
+  const newAddress = page.getByRole("radio", {
+    name: "Nueva dirección",
+    exact: true,
+  });
+  if (await newAddress.isVisible().catch(() => false)) {
+    await newAddress.check();
+  }
+  await checkout.fillAddress(options.address || testData.address);
+
+  const saveAddress = page.getByRole("checkbox", {
+    name: /Guardar datos de env[ií]o en Mi cuenta/i,
+  });
+  if (await saveAddress.isVisible().catch(() => false) && await saveAddress.isChecked()) {
+    await saveAddress.uncheck();
+  }
+
+  await checkout.selectShippingMethod();
+  await checkout.acceptTerms();
+  await checkout.continueToPayment({ expectedPaymentMode: options.expectedPaymentMode });
+  const payment = new PaymentPage(page);
+  await payment.validatePaymentPage({ expectedPaymentMode: options.expectedPaymentMode });
+  return { cart, checkout, payment };
 }
