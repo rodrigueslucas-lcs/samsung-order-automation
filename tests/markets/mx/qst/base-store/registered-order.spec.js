3552fa5db822b9983e3451f0f2a1b225dfee63d8
Lucas Rodrigues <afolucas@gmail.com>
2026-09-25 11:09:05 -0300

    refactor(reporting): move MX registered order evidence import

--- tests/markets/mx/qst/base-store/registered-order.spec.js
+++ tests/markets/mx/qst/base-store/registered-order.spec.js
@@ -1,7 +1,7 @@
 import MarketPaymentPage from "../../../../../pages/MarketPaymentPage";
 import MxCheckoutPage from "../../../../../pages/MxCheckoutPage";
 import destructiveGuards from "../../../../../utils/destructiveGuards";
-import evidenceContext from "../../../../../reporters/evidence/evidenceContext.js";
+import evidenceContext from "../../../../../reporting/evidence/evidenceContext.js";
 import qstEvidenceMetadata from "../../../../../utils/qstEvidenceMetadata.js";
 import mxTestCard from "../../../../../utils/mxTestCard.js";
 import { test, expect } from "../../dst/base-store/mx.auth.fixture";
